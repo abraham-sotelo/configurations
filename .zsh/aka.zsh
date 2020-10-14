1b96c7371c6c54ef3fc82a93260241c73849c068
Abraham <asoteloa@outlook.com>
2020-10-14 18:59:45 -0500

    zsh - Git and Docker aliases

---
+++ .zsh/aka.zsh
@@ -0,0 +1,14 @@
+#Git aliases
+alias g='git'
+alias gs='git status'
+alias gd='git diff'
+alias gl='git log'
+alias gr='git remote --verbose'
+
+#Docker aliases
+alias d='docker'
+alias dps='docker ps'
+alias dpsa='docker ps -a'
+alias dc='docker container'
+alias di='docker image'
+alias dils='docker image ls'
