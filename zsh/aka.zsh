04f005bf9ccb76cddcf37bef822c583ae59d2a55
Abraham Sotelo <abraham.sotelo@motional.com>
2022-10-20 13:16:54 +0800

    zsh - Kubernetes alias

--- zsh/aka.zsh
+++ zsh/aka.zsh
@@ -1,17 +1,24 @@
-#Git aliases
+# Git aliases
 alias g='git'
 alias gs='git status'
 alias gd='git diff'
 alias gl='git log --oneline -n'
 alias gr='git remote --verbose'
 
-#Docker aliases
+# Docker aliases
 alias d='docker'
 alias dps='docker ps'
 alias dpsa='docker ps -a'
 alias dc='docker container'
 alias di='docker image'
 alias dils='docker image ls'
+
+#Kubernetes aliases
+alias k='kubectl'
+alias kpod='kubectl get pods'
+
+# Shell aliases
+alias ls='ls --color'
 
 #NeoVim aliases
 #alias vim='chmod u+x ~/.nvim/nvim.appimage && ~/.nvim/nvim.appimage'
